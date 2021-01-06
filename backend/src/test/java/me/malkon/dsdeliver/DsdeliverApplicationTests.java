87a6607c18221c7ba095218c37061a7ed794e5ac
Malkon Faria <sudo777@protonmail.com>
2021-01-06 07:50:08 -0300

    First commit

---
+++ backend/src/test/java/me/malkon/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package me.malkon.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
