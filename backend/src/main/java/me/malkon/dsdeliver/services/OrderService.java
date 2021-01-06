a46736fb1240c755171c51799df1abc402187f4b
Malkon Faria <sudo777@protonmail.com>
2021-01-06 15:00:06 -0300

    Created POST for orders

--- backend/src/main/java/me/malkon/dsdeliver/services/OrderService.java
+++ backend/src/main/java/me/malkon/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package me.malkon.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import me.malkon.dsdeliver.dto.OrderDto;
+import me.malkon.dsdeliver.dto.ProductDto;
 import me.malkon.dsdeliver.entities.Order;
+import me.malkon.dsdeliver.entities.OrderStatus;
+import me.malkon.dsdeliver.entities.Product;
 import me.malkon.dsdeliver.repositories.OrderRepository;
+import me.malkon.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,11 +22,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
-	public List<OrderDto> findAll(){
+	public List<OrderDto> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDto(x)).collect(Collectors.toList());
 	}
 
+	@Transactional
+	public OrderDto insert(OrderDto dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLongitude(), dto.getLatitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDto p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDto(order);
+	}
 
 }
