8b8f82c3133cc0e48a279ad3daf6aac5df9ef80e
Malkon Faria <sudo777@protonmail.com>
2021-01-10 08:37:31 -0300

    Add env vars

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
 import Routes from './Routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
