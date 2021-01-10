84d14b7fc043e5c8fe94b970b35e506e855373a3
Malkon Faria <sudo777@protonmail.com>
2021-01-10 08:13:56 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,11 +2,14 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import Routes from './Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <>
       <Routes />
+      <ToastContainer />
     </>
   );
 }
--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
 const apiUrl = "http://localhost:8080";
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
@@ -10,4 +11,8 @@ export function fetchProducts(){
 //busca o endereço
 export function fetchLocalMapBox(local: string ){
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayload){
+    return axios.post(`${apiUrl}/orders`, payload);
 }
